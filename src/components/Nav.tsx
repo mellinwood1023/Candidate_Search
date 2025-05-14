23eca90d5bbcf0286cd70c2920478eb6f3272966
Maggie Ellinwood <mellinwood1023@gmail.com>
2025-05-14 18:12:26 -0400

    remove nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,7 +4,6 @@ const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
     <nav style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
-    <div>Nav</div>
     <ul style={{ listStyle: 'none', display: 'flex', gap: '10px', padding: 0 }}>
       <li>
         <Link to="/">Home</Link>
